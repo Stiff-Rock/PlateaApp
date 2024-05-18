3ef415f68319d89fd1b145a66dfdd382ecade660
HugoOsma_ExamenGit <perdiceman@gmail.com>
2024-05-18 15:45:55 +0200

    Perfil hacer

--- src/App/Perfil.java
+++ src/App/Perfil.java
@@ -1,43 +1,111 @@
 package App;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
-public class Perfil {
+public class Perfil extends JFrame {
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
 
-	private JFrame frame;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Perfil window = new Perfil();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		PlateaLanzadera window = new PlateaLanzadera();
+		window.setVisible(true);
 	}
 
-	/**
-	 * Create the application.
-	 */
 	public Perfil() {
-		initialize();
+		setBounds(100, 100, 1280, 720);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(313, 59, 664, 547);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(207, 207, 243));
+		panel_2.setBounds(79, 154, 493, 304);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Nombre y apellidos");
+		lblNewLabel.setBounds(28, 33, 109, 13);
+		panel_2.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(28, 46, 432, 19);
+		panel_2.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Direccion:");
+		lblNewLabel_1.setBounds(28, 91, 84, 13);
+		panel_2.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(28, 104, 432, 19);
+		panel_2.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(28, 163, 432, 19);
+		panel_2.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(28, 220, 432, 19);
+		panel_2.add(textField_3);
+		
+		JLabel lblNewLabel_2 = new JLabel("CP");
+		lblNewLabel_2.setBounds(28, 150, 45, 13);
+		panel_2.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Localidad");
+		lblNewLabel_3.setBounds(28, 208, 45, 13);
+		panel_2.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Ninckname");
+		lblNewLabel_4.setBounds(171, 78, 145, 47);
+		panel.add(lblNewLabel_4);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(162, 196, 201));
+		panel_1.setBounds(0, 0, 185, 683);
+		getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Home");
+		btnNewButton.setBounds(42, 107, 100, 40);
+		panel_1.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Mi perfil");
+		btnNewButton_1.setBounds(42, 173, 100, 40);
+		panel_1.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Publicaciones");
+		btnNewButton_2.setBounds(42, 237, 100, 40);
+		panel_1.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Favoritos");
+		btnNewButton_3.setBounds(42, 308, 100, 40);
+		panel_1.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("Votados");
+		btnNewButton_4.setBounds(42, 384, 100, 40);
+		panel_1.add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("Notificaciones");
+		btnNewButton_5.setBounds(42, 460, 100, 40);
+		panel_1.add(btnNewButton_5);
 	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
 }
+
