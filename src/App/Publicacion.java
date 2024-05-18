2cede05a1d2165c67a94723fd2ede2043f1c1f40
HugoOsma_ExamenGit <perdiceman@gmail.com>
2024-05-18 15:07:09 +0200

    Publicacion es la base, no he puesto logo pero los colores son los mismos,y tampoco he puesto los simbolos

---
+++ src/App/Publicacion.java
@@ -0,0 +1,58 @@
+package App;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.JScrollPane;
+
+public class Publicacion extends JFrame {
+
+
+	public static void main(String[] args) {
+		PlateaLanzadera window = new PlateaLanzadera();
+		window.setVisible(true);
+	}
+
+	public Publicacion() {
+		setBounds(100, 100, 1280, 720);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(207, 207, 243));
+		panel.setBounds(313, 59, 664, 547);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(162, 196, 201));
+		panel_1.setBounds(0, 0, 185, 683);
+		getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(42, 107, 100, 40);
+		panel_1.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(42, 173, 100, 40);
+		panel_1.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.setBounds(42, 237, 100, 40);
+		panel_1.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		btnNewButton_3.setBounds(42, 308, 100, 40);
+		panel_1.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("New button");
+		btnNewButton_4.setBounds(42, 384, 100, 40);
+		panel_1.add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("New button");
+		btnNewButton_5.setBounds(42, 460, 100, 40);
+		panel_1.add(btnNewButton_5);
+	}
+}
